a1909de3a74b7b56bcb5180204ff25ec708e4ee6
Nurtas <rahmatullaevnurtas@gmail.com>
2021-03-06 16:57:41 +0600

    HW 1 - even, odd, sum of array

---
+++ ConsoleApp/HomeWork_1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KaspiLab_HW
+{
+    class HomeWork_1
+    {
+        public static void IterateArray(int[] arr)
+        {
+            int sum = 0;
+            var (even, odd) = (String.Empty, String.Empty);
+
+            foreach(var num in arr)
+            {
+                sum += num;
+                if (num % 2 == 0) even += $" {num.ToString()}";
+                else odd += $" {num.ToString()}"; 
+            }
+
+            Console.WriteLine($"Even numbers {even.Trim()}");
+            Console.WriteLine($"Odd numbers {odd.Trim()}");
+            Console.WriteLine($"Sum of array is {sum}");
+        }
+    }
+}
