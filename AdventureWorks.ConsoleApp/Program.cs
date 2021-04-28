d2e7e04dd19b7f6b953a3908a939a3deab5f0a81
Nurtas <rahmatullaevnurtas@gmail.com>
2021-04-28 15:00:13 +0600

    EF

---
+++ AdventureWorks.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using AdventureWorks.EF.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Types;
+
+namespace AdventureWorks.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new AWContext())
+            {
+                var t = db.Document.ToList();
+            }
+        }
+    }
+}
---
+++ AdventureWorks.EF/Models/Document.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Hierarchy;
+
+namespace AdventureWorks.EF.Models
+{
+    [Table("Production.Document")]
+    public partial class Document
+    {
+        public Document()
+        {
+        }
+
+        [Key]
+        [Required]
+        public HierarchyId DocumentNode { get; set; }
+        public int? DocumentLevel { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public int Owner { get; set; }
+        [Required]
+        public bool FolderFlag { get; set; }
+        [Required]
+        public string FileName { get; set; }
+        [Required]
+        public string FileExtension { get; set; }
+        [Required]
+        public string Revision { get; set; }
+        [Required]
+        public int ChangeNumber { get; set; }
+        [Required]
+        public int Status { get; set; }
+        [Required]
+        public string DocumentSummary { get; set; }
+        [Column("Document")]
+        public byte[] DocumentBinary { get; set; }
+        [Required]
+        public Guid rowguid { get; set; }
+        [Required]
+        public DateTime ModifiedDate { get; set; }
+    }
+}
