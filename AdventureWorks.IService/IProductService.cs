9804be3966dd17e0ff4525f7b587dec43d706f1f
stillnurtas <rahmatullaevnurtas@gmail.com>
2021-05-18 17:48:08 +0600

    added WCF self host service

---
+++ AdventureWorks.IService/IProductService.cs
@@ -0,0 +1,24 @@
+﻿using AdventureWorks.DTO.Models.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorks.IService
+{
+    [ServiceContract]
+    public interface IProductService
+    {
+        [OperationContract]
+        Task<IEnumerable<SCProductDTO>> GetProducts(int pageIndex, int pageSize);
+
+        [OperationContract]
+        Task<ProductDetailsDTO> GetDetails(int productId);
+        
+        [OperationContract]
+        Task<byte[]> GetImage(int productId);
+    }
+}
